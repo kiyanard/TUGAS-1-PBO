1a8522891a16617222a6ae9813c37e840a992f3d
kiyanard <111202113531@mhs.dinus.ac.id>
2023-03-10 22:54:54 +0700

    commit baru

--- Identitas.java
+++
@@ -1,8 +0,0 @@
-public class Identitas {
-    public static void main(String[] args){
-        String nama = "Ivan";
-        String nim  = "A11.2021.13531";
-        System.out.println("Nama saya adalah " +nama);
-        System.out.print("NIM saya " +nim);
-    }
-}
