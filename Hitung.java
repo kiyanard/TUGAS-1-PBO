1375ec1ff9a6600afc97334eabd141c4f83a5b7d
kiyanard <111202113531@mhs.dinus.ac.id>
2023-03-10 19:52:41 +0700

    first commit

---
+++ Hitung.java
@@ -0,0 +1,7 @@
+public class Hitung {
+    public static void main(String[] args){
+        int a=10,b=13;
+        int x= a+b;
+        System.out.print(x);
+    }
+}
---
+++ Identitas.java
@@ -0,0 +1,8 @@
+public class Identitas {
+    public static void main(String[] args){
+        String nama = "Ivan";
+        String nim  = "A11.2021.13531";
+        System.out.println("Nama saya adalah " +nama);
+        System.out.print("NIM saya " +nim);
+    }
+}
